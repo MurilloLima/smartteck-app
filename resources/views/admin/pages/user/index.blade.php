3fb8ecbe884bddcfd4dda61a714108c9204349be
Murillo Lima <murillo_smit@hotmail.com>
2024-12-09 08:59:28 -0300

    layout user

--- resources/views/admin/pages/user/index.blade.php
+++ resources/views/admin/pages/user/index.blade.php
@@ -18,28 +18,44 @@
     </section>
 
     <section class="content">
-        <!-- Default box -->
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Dados</h3>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
 
-                {{-- <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                        <i class="fas fa-times"></i>
-                    </button>
-                </div> --}}
+                    <!-- Profile Image -->
+                    <div class="card card-primary card-outline">
+                        <div class="card-body box-profile">
+                            <div class="text-center">
+                                <img class="profile-user-img img-fluid img-circle" src="{{ asset('admin/dist/img/user4-128x128.jpg') }}"
+                                    alt="User profile picture">
+                            </div>
+
+                            <h3 class="profile-username text-center">{{ Auth::user()->name }}</h3>
+
+                            <p class="text-muted text-center">Administrador</p>
+
+                            <ul class="list-group list-group-unbordered mb-3">
+                                <li class="list-group-item">
+                                    <b>Followers</b> <a class="float-right">1,322</a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>Following</b> <a class="float-right">543</a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>Friends</b> <a class="float-right">13,287</a>
+                                </li>
+                            </ul>
+
+                            <a href="#" class="btn btn-primary"><b>Salvar</b></a>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
             </div>
-            <div class="card-body">
-                <form action="" method="post">
-                    <label for="">*Nome do administrador:</label><br>
-                    <input type="text" name="" class=""><br>
-                    <button type="submit" class="btn btn-primary">Salvar</button>
-                </form>
-            </div>
-        </div>
-        <!-- /.card -->
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
     </section>
 @endsection
